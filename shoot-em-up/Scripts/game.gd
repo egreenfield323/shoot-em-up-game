5b0256333bce68bd0a5c67aef49d313a958d9365
Evan Greenfield <evangreenfield@live.com>
2024-09-10 11:51:24 -0400

    Add game background
    Added a space game background that scales with the window size

--- shoot-em-up/Scripts/game.gd
+++ shoot-em-up/Scripts/game.gd
@@ -5,3 +5,12 @@ var score = 0
 func incrementScore():
 	score += 1
 	get_node("score").text = str(score)
+
+func _process(delta: float) -> void:
+	var scaleX = DisplayServer.window_get_size().x / $world_background.texture.get_size().x
+	var scaleY = DisplayServer.window_get_size().y / $world_background.texture.get_size().y
+	
+	$world_background.set_scale(Vector2(scaleX, scaleY))
+	$world_background.position = Vector2((DisplayServer.window_get_size().x) / 2,(DisplayServer.window_get_size().y) / 2)
+	
+	pass
